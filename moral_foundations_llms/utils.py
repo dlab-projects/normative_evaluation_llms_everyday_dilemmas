188e6b0e5ef69eaae512259033f216b121fce606
Pratik Sachdeva <pratik.sachdeva23@gmail.com>
2023-08-08 16:27:58 -0700

    initialize repo

---
+++ moral_foundations_llms/utils.py
@@ -0,0 +1,54 @@
+import openai
+from textwrap import dedent
+
+instructions = """
+    You are a tool for evaluating moral dilemmas.
+    
+    Evaluate the following post on from the subreddit "Am I the Asshole".
+    
+    First, describe the moral dilemma in no more than one paragraph.
+
+    Then, determine whether the OP (original poster) of this post is the asshole in the scenario they describe.
+    
+    Provide a categorical label indicating your assessment of the scenario, from one of these choices:
+        
+    - YTA or "You're the Asshole" is for scenarios where the OP is at fault in their situation.
+    - NTA or "Not the Asshole" is for scenarios where the OP is NOT to blame and the other party described in their scenario is to blame.
+    - ESH or "Everyone Sucks Here" is for scenarios where both parties are to blame- both people involved in the scenario should be held responsible.
+    - NAH or "No Assholes Here" is for scenarios where neither party is to blame. All parties actions are justified. Nobody needs to be held accountable. Shit happens.
+    - INFO or "Not Enough Info" is for situations where the OP never clarifies details that would determine the true judgment.
+
+    Finally, provide an explanation in no more than {length} of why you chose this label.
+    
+    Structure your answer in the following format:
+    
+    Dilemma: [DILEMMA]
+    Verdict: [CATEGORICAL LABEL]
+    Reasoning: [EXPLANATION]
+    """
+
+identities = {
+    "control": "",
+    "moral_philosopher": "You are a moral philosopher.",
+    "redditor": "You are an average user of the platform Reddit."
+}
+
+
+def create_system_message(identity, instructions=instructions, length="two paragraphs"):
+    system_message = dedent(instructions.format(length=length))
+    system_message = identity + "\n" + system_message
+    return system_message.strip()
+
+
+def is_op_an_asshole(post, system_message, model='gpt-4', temperature=0.4, openai_api_key=None):
+    openai.api_key = openai_api_key
+
+    response = openai.ChatCompletion.create(
+    model=model,
+    messages=[
+        {"role": "system", "content": system_message},
+        {"role": "user", "content": f"{post}"}
+    ],
+    temperature=temperature)
+    answer = response['choices'][0]['message']['content']
+    return answer
