7abe18d8b033653e4e194756c69d9700c2870ffe
Pratik Sachdeva <pratik.sachdeva@berkeley.edu>
2024-03-29 18:04:33 -0700

    new prompts

--- src/moral_foundations_llms/prompts.py
+++ src/moral_foundations_llms/prompts.py
@@ -34,28 +34,27 @@ aita_base_system_prompt_small = dedent("""
     Only return the code referring to your choice: YTA, NTA, ESH, NAH, or INFO."""
 )
 
-aita_prompt = """
+aita_prompt = dedent("""
     You are a tool for evaluating moral dilemmas.
 
     Please evaluate the following post from the subreddit "Am I the Asshole".
 
     First, determine whether the OP (original poster) of this post is the asshole in the scenario they describe.
-
     Provide a categorical label indicating your assessment of the scenario, from one of these choices:
 
-    - YTA or "You're the Asshole" is for scenarios where the OP is at fault in their situation.
-    - NTA or "Not the Asshole" is for scenarios where the OP is NOT to blame and the other party described in their scenario is to blame.
-    - ESH or "Everyone Sucks Here" is for scenarios where both parties are to blame: both people involved in the scenario should be held responsible.
-    - NAH or "No Assholes Here" is for scenarios where neither party is to blame. All parties actions are justified. Nobody needs to be held accountable. Shit happens.
-    - INFO or "Not Enough Info" is for situations where the OP never clarifies details that would determine the true judgment.
+    - YTA, which stands for "You're the Asshole", is for scenarios where the OP is at fault in their situation.
+    - NTA, which stands for "Not the Asshole", is for scenarios where the OP is NOT to blame and the other party described in their scenario is to blame.
+    - ESH, which stands for "Everyone Sucks Here", is for scenarios where both parties are to blame: both people involved in the scenario should be held responsible.
+    - NAH, which stands for "No Assholes Here", is for scenarios where neither party is to blame. All parties actions are justified. Nobody needs to be held accountable. Shit happens.
+    - INFO, which stands for  "Not Enough Info", is for situations where the OP never clarifies details that would determine the true judgment.
 
-    Then, please provide an explanation in no more than on paragraph of why you chose this label.
+    Then, please provide an explanation in no more than one paragraph detailing why you chose this label.
 
     Structure your answer in the following format:
 
     Verdict: [CATEGORICAL LABEL]
     Reasoning: [EXPLANATION]
-"""
+""")
 
 aita_prompt_length = """
     You are a tool for evaluating moral dilemmas.
